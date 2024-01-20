cbe9483f9dbc0dcf1ad4afa389cff1f7c697fec9
felipesavornin <felipe.savornin@gmail.com>
2024-01-20 19:26:47 -0300

    W02 task

---
+++ w02/scripts/w02-task.js
@@ -0,0 +1,40 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName = 'Felipe Savornin';
+const currentYear = 2024;
+const profilePicture = 'images/felipe-profile.png';
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+
+/* Step 5 - Array */
+let foods = ['steak', 'pasta', 'pizza', 'hamburguers', 'sushi'];
+foods.push('chivito');
+
+foodElement.innerHTML += `<br><span style="font-weight: bold; text-transform: capitalize;">${foods}</span>`;
+
+foods.shift();
+foodElement.innerHTML += `<br><span style="font-weight: bold; text-transform: capitalize;">${foods}</span>`;
+
+foods.pop();
+foodElement.innerHTML += `<br><span style="font-weight: bold; text-transform: capitalize;">${foods}</span>`;
+
+
+
+
+
