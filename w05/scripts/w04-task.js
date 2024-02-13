d8f32f8db72d64024484ff167853b7eb308f541e
felipesavornin <felipe.savornin@gmail.com>
2024-02-13 02:29:17 -0300

    w05

---
+++ w05/scripts/w04-task.js
@@ -0,0 +1,81 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: 'Felipe Savornin',
+    photo: './images/felipe-profile.png',
+    favoriteFoods: [
+        'Steak', 
+        'Pasta', 
+        'Pizza', 
+        'Hamburguers', 
+        'Sushi'
+    ],
+    hobbies: [
+        'Play soccer',
+        'Go to the movies'
+    ],
+    placesLived: []
+};
+
+/* Populate Profile Object with placesLive objects */
+
+myProfile.placesLived.push(
+    {
+        place: 'Montevideo, Uruguay',
+        length: '12 years'
+    }
+);
+
+myProfile.placesLived.push(
+    {
+        place: 'Neuquen, Argentina',
+        length: '2 years'
+    }
+);
+
+myProfile.placesLived.push(
+    {
+        place: 'Paysandu, Uruguay',
+        length: '19 years'
+    }
+);
+
+/* DOM Manipulation - Output */
+
+/* Name */
+document.querySelector('#name').textContent = myProfile.name;
+
+/* Photo with attributes */
+document.querySelector('#photo').src = myProfile.photo;
+document.querySelector('#photo').alt = myProfile.name;
+
+/* Favorite Foods List*/
+myProfile.favoriteFoods.forEach(food => {
+        let li = document.createElement('li');
+        li.textContent = food;
+        document.querySelector('#favorite-foods').appendChild(li);
+    }
+);
+
+/* Hobbies List */
+myProfile.hobbies.forEach(hobbie => {
+        let li = document.createElement('li');
+        li.textContent = hobbie;
+        document.querySelector('#hobbies').appendChild(li);
+    }
+);
+
+/* Places Lived DataList */
+myProfile.placesLived.forEach(place => {
+        let dt = document.createElement('dt');
+        dt.textContent = place.place;
+
+        let dd = document.createElement('dd');
+        dd.textContent = place.length;
+
+        document.querySelector('#places-lived').appendChild(dt);
+        document.querySelector('#places-lived').appendChild(dd);        
+    }
+);
+
---
+++ w05/scripts/w05-task.js
@@ -0,0 +1,86 @@
+/* W05: Programming Tasks */
+
+/* Declare and initialize global variables */
+const templesElement = document.getElementById('temples');
+let templeList = [];
+
+/* async displayTemples Function */
+const displayTemples = (temples) => {
+    temples.forEach(element => {
+
+        let card = document.createElement('article');
+
+        let name = document.createElement('h3');
+        name.innerHTML = element.templeName;
+        card.appendChild(name);
+
+        let img = document.createElement('img');
+        img.src = element.imageUrl;
+        img.alt = element.location;
+        card.appendChild(img);
+
+        templesElement.appendChild(card);
+    });
+};
+
+/* async getTemples Function using fetch()*/
+const getTemples = async () => {
+    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+
+    if (response.ok) {
+        templeList = await response.json();
+        displayTemples(templeList);
+    }
+};
+
+/* reset Function */
+const reset = () => {
+    templesElement.innerHTML = '';
+};
+
+/* filterTemples Function */
+const filterTemples = function(temples){
+    reset();
+    let filter = document.getElementById('filtered').value;
+    let newTempleArray = [];
+
+    switch(filter) {
+        case 'utah':
+            newTempleArray = temples.filter(filTemp);
+            function filTemp(i){                
+                if(i.location.includes('Utah')){
+                    return i;
+                }
+            }
+            displayTemples(newTempleArray);
+        break;
+
+        case 'notutah':
+            newTempleArray = temples.filter(filNoUtah);
+            function filNoUtah(i){             
+                if(!i.location.includes('Utah')){
+                    return i;
+                }
+            }
+            displayTemples(newTempleArray);
+        break;
+
+        case 'older':
+            newTempleArray = temples.filter(filNoUtah);
+            function filNoUtah(i){       
+                if(Date.parse(i.dedicated) < new Date(1950, 0, 1)){
+                    return i;
+                }
+            }
+            displayTemples(newTempleArray);
+        break;
+
+        case 'all':
+            displayTemples(temples);
+    }
+}
+
+getTemples();
+
+/* Event Listener */
+document.querySelector("#filtered").addEventListener("change", () => { filterTemples(templeList) });
